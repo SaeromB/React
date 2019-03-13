2d1607ff2dbabe0174df7d57739c98ca9f219e28
Saerom Bang <saerombang11@gmail.com>
2019-03-13 16:17:30 +0900

    component part two edit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import './App.css';
 import { DISHES } from './shared/dishes';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
       dishes: DISHES
     };
